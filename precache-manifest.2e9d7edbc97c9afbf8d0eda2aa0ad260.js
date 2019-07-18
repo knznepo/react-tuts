7187efb817cbd9ce96d7125e7900cfb323a76473
Kenaz Nepomuceno <kenaznepomuceno@yahoo.com>
2019-07-18 18:06:01 +0800

    Updates

---
+++ precache-manifest.2e9d7edbc97c9afbf8d0eda2aa0ad260.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "46bce32d2866f7d793a66e87e31bc6bd",
+    "url": "/react-tutorial/index.html"
+  },
+  {
+    "revision": "a3e4d886524e5616c83e",
+    "url": "/react-tutorial/static/css/main.c8b44c70.chunk.css"
+  },
+  {
+    "revision": "a397e4f191bc78c9bd60",
+    "url": "/react-tutorial/static/js/2.c40744a1.chunk.js"
+  },
+  {
+    "revision": "a3e4d886524e5616c83e",
+    "url": "/react-tutorial/static/js/main.794158ff.chunk.js"
+  },
+  {
+    "revision": "2a4d04de927634e03e5b",
+    "url": "/react-tutorial/static/js/runtime~main.782a6e4d.js"
+  }
+]);
